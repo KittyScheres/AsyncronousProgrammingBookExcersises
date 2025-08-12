d42d6cfb2a49c74d2af6b350e23a3a66cd449dfe
Kitty <kittyhwscheres@gmail.com>
2025-08-12 18:03:52 +0200

    fixed cout syncing section

--- Excersises/Chapter3/ThreadSyncronization.cpp
+++ Excersises/Chapter3/ThreadSyncronization.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+
+#include <syncstream>
 #include <thread>
 
 namespace Chapter3 {
@@ -6,13 +8,13 @@ namespace Chapter3 {
 		void RaceCondition() {
 			std::thread t1 = std::thread([](){
 				for (int i = 0; i < 100; i++) {
-					printf("1 2 3 4\n");
+					std::osyncstream(std::cout) << "1" << "2" << "3" << "4" << std::endl;
 				}
 			});
 
 			std::thread t2 = std::thread([]() {
 				for (int i = 0; i < 100; i++) {
-					printf("5 6 7 8\n");
+					std::osyncstream(std::cout) << "5" << "6" << "7" << "8" << std::endl;
 				}
 			});
 
